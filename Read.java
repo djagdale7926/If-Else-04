7184847f5375be89bc733b5527b9fed1353504f4
djagdale7926 <dnyaneshwarijagadale22@gmail.com>
2025-10-07 16:59:27 +0530

    Add files via upload

---
+++ Read.java
@@ -0,0 +1,386 @@
+class Attendence{
+    public static void main(String[] args){
+        float attendence=64.44f;
+        if(attendence>=75){
+            System.out.println("Allowed for exam");
+        }
+        else{
+            System.out.println("Not allowed for exam");
+        }
+    }
+}
+
+class Triplet2{
+    public static void main(String[] args){
+        int a=2;
+        int b=2;
+        int c=2;
+        if(a*a+b*b==c*c){
+            System.out.println("Its pythagorean triplet");
+        }
+        else{
+            System.out.println("Not a pythagorean triplet"); 
+        }
+    }
+}
+
+class Triplet1{
+    public static void main(String[] args){
+        int a=3;
+        int b=4;
+        int c=5;
+        if(a*a+b*b==c*c){
+            System.out.println("Its pythagorean triplet");
+        }
+        else{
+            System.out.println("Not a pythagorean triplet"); 
+        }
+    }
+}
+
+class Marks5{
+    public static void main(String[] args){
+        float percent=47f;
+        if(percent>=75.00){
+            System.out.println("First class with distinction");
+        }
+        else if(percent<75.00 && percent>=60.00){
+            System.out.println("First class");
+        }
+        else if(percent<60.00 && percent>=50.00){
+            System.out.println("Second class");
+        }
+        else if(percent<50.00 && percent>=35.00){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("Fail");
+        }
+        }
+}
+
+
+class Marks4{
+    public static void main(String[] args){
+        float percent=54f;
+        if(percent>=75.00){
+            System.out.println("First class with distinction");
+        }
+        else if(percent<75.00 && percent>=60.00){
+            System.out.println("First class");
+        }
+        else if(percent<60.00 && percent>=50.00){
+            System.out.println("Second class");
+        }
+        else if(percent<50.00 && percent>=35.00){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("Fail");
+        }
+        }
+}
+
+
+class Marks3{
+    public static void main(String[] args){
+        float percent=60f;
+        if(percent>=75.00){
+            System.out.println("First class with distinction");
+        }
+        else if(percent<75.00 && percent>=60.00){
+            System.out.println("First class");
+        }
+        else if(percent<60.00 && percent>=50.00){
+            System.out.println("Second class");
+        }
+        else if(percent<50.00 && percent>=35.00){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("Fail");
+        }
+        }
+}
+
+
+class Marks2{
+    public static void main(String[] args){
+        float percent=75f;
+        if(percent>=75.00){
+            System.out.println("First class with distinction");
+        }
+        else if(percent<75.00 && percent>=60.00){
+            System.out.println("First class");
+        }
+        else if(percent<60.00 && percent>=50.00){
+            System.out.println("Second class");
+        }
+        else if(percent<50.00 && percent>=35.00){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("Fail");
+        }
+        }
+}
+
+
+class Marks1{
+    public static void main(String[] args){
+        float percent=85f;
+        if(percent>=75.00){
+            System.out.println("First class with distinction");
+        }
+        else if(percent<75.00 && percent>=60.00){
+            System.out.println("First class");
+        }
+        else if(percent<60.00 && percent>=50.00){
+            System.out.println("Second class");
+        }
+        else if(percent<50.00 && percent>=35.00){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("Fail");
+        }
+        }
+}
+
+class ProfitLoss3{
+    public static void main(String[] args){
+        int sellingprice=900;
+        int costprice=900;
+        int Price=sellingprice-costprice;
+        if(Price>0){
+            System.out.println("Profit of " + Price);
+        }
+        else if(Price<0){
+            System.out.println("Loss of " + (-Price));
+        }
+        else{
+            System.out.println("No profit no loss");
+        }
+    }
+}
+
+class ProfitLoss2{
+    public static void main(String[] args){
+        int sellingprice=300;
+        int costprice=500;
+        int Price=sellingprice-costprice;
+        if(Price>0){
+            System.out.println("Profit of " + Price);
+        }
+        else if(Price<0){
+            System.out.println("Loss of " + (-Price));
+        }
+        else{
+            System.out.println("No profit no loss");
+        }
+    }
+}
+
+class ProfitLoss1{
+    public static void main(String[] args){
+        int sellingprice=1200;
+        int costprice=1000;
+        int Price=sellingprice-costprice;
+        if(Price>0){
+            System.out.println("Profit of " + Price);
+        }
+        else if(Price<0){
+            System.out.println("Loss of " + (-Price));
+        }
+        else{
+            System.out.println("No profit no loss");
+        }
+    }
+}
+
+class Demo14{
+    public static void main(String[] args){
+        int age=-31;
+        if(age>=18){
+            System.out.println("Valid age for voting");
+        }
+        else{
+            System.out.println("Invalid age");
+        }
+    }
+}
+
+class Demo13{
+    public static void main(String[] args){
+        int age=23;
+        if(age>=18){
+            System.out.println("Valid age for voting");
+        }
+        else{
+            System.out.println("Invalid age");
+        }
+    }
+}
+
+class Demo12{
+    public static void main(String[] args){
+        float num=89.96f;
+        if(num%6==0){
+            System.out.println(num + " is divisible by 6");
+        }
+        else{
+            System.out.println(num + " is not divisible by 6");
+        }
+    }
+}
+
+
+class Demo11{
+    public static void main(String[] args){
+        float num=120f;
+        if(num%6==0){
+            System.out.println(num + " is divisible by 6");
+        }
+        else{
+            System.out.println(num + " is not divisible by 6");
+        }
+    }
+}
+
+class Demo10{
+    public static void main(String[] args){
+        float percentage=69.87f;
+        if(percentage>85.00){
+            System.out.println("Medical");
+        }
+        else if(percentage<=85.00 && percentage>75.00){
+            System.out.println("Engineering");
+        }
+        else if(percentage<=75.00 && percentage>65.00){
+            System.out.println("pharmacy or bachelaor in science");
+        }
+        else{
+            System.out.println("BA");
+        }
+    }
+}
+
+class Demo9{
+    public static void main(String[] args){
+        float percentage=80f;
+        if(percentage>85.00){
+            System.out.println("Medical");
+        }
+        else if(percentage<=85.00 && percentage>75.00){
+            System.out.println("Engineering");
+        }
+        else if(percentage<=75.00 && percentage>65.00){
+            System.out.println("pharmacy or bachelaor in science");
+        }
+        else{
+            System.out.println("BA");
+        }
+    }
+}
+
+class Demo8{
+    public static void main(String[] args){
+        float percentage=95f;
+        if(percentage>85.00){
+            System.out.println("Medical");
+        }
+        else if(percentage<=85.00 && percentage>75.00){
+            System.out.println("Engineering");
+        }
+        else if(percentage<=75.00 && percentage>65.00){
+            System.out.println("pharmacy or bachelaor in science");
+        }
+        else{
+            System.out.println("BA");
+        }
+    }
+}
+
+class Demo7{
+    public static void main(String[] args){
+        char ch='D';
+        if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+            System.out.println(ch + " is Vowel");
+        }
+        else{
+            System.out.println(ch + " is Consonant");
+        }
+    }
+}
+
+class Demo6{
+    public static void main(String[] args){
+        char ch='a';
+        if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+            System.out.println(ch + " is Vowel");
+        }
+        else{
+            System.out.println(ch + " is Consonant");
+        }
+    }
+}
+
+class Demo5{
+    public static void main(String[] args){
+        int num =144;
+        if (num%13==0){
+            System.out.println(num + " is in the table of 13");
+        }
+        else{
+            System.out.println(num + " is not in the table of 13");
+        }
+    }
+}
+
+class Demo4{
+    public static void main(String[] args){
+        int num =156;
+        if (num%13==0){
+            System.out.println(num + " is in the table of 13");
+        }
+        else{
+            System.out.println(num + " is not in the table of 13");
+        }
+    }
+}
+
+class Demo3{
+    public static void main(String[] args){
+        int num =-11;
+        if (num>=1 && num<=1000){
+            System.out.println(num + " is in the range 1 to 1000");
+        }
+        else{
+            System.out.println(num + " is not in the range 1 to 1000");
+        }
+    }
+}
+
+class Demo2{
+    public static void main(String[] args){
+        int num =0;
+        if (num>=1 && num<=1000){
+            System.out.println(num + " is in the range 1 to 1000");
+        }
+        else{
+            System.out.println(num + " is not in the range 1 to 1000");
+        }
+    }
+}
+
+class Demo1{
+    public static void main(String[] args){
+        int num =100;
+        if (num>=1 && num<=1000){
+            System.out.println(num + " is in the range 1 to 1000");
+        }
+        else{
+            System.out.println(num + "is not in the range 1 to 1000");
+        }
+    }
+}
